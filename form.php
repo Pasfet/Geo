5e6ba6716337edf444448e9fc418f8d74d6aa181
Pavel <49246093+Pasfet@users.noreply.github.com>
2019-04-29 22:39:56 +0300

    1

--- form.php
+++ form.php
@@ -1,7 +1,7 @@
 <?php 
 if(isset($_POST['submit'])){
     /* Устанавливаем e-mail Кому и от Кого будут приходить письма */   
-    $to = "i@u53335.onhh.ru"; // Здесь нужно написать e-mail, куда будут приходить письма   
+    $to = "info@geoskyspb.ru"; // Здесь нужно написать e-mail, куда будут приходить письма   
     $from = $_POST['email-one'];
 
 $name = $_POST['name-one'];
@@ -22,11 +22,11 @@ $headers2 = "From:" . $to;
 mail($to,$subject,$mail_to,$headers);
 // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
 echo "Сообщение отправлено. Спасибо Вам " . $name . ", мы скоро свяжемся с Вами.";
-echo "<br /><br /><a href='http://u53335.onhh.ru/'>Вернуться на сайт.</a>";
+echo "<br /><br /><a href='https://geoskyspb.ru'>Вернуться на сайт.</a>";
 }
 ?>
 <!--Переадресация на главную страницу сайта, через 3 секунды-->
 <script language="JavaScript" type="text/javascript">
-function changeurl(){eval(self.location="http://u53335.onhh.ru/");}
+function changeurl(){eval(self.location="https://geoskyspb.ru");}
 window.setTimeout("changeurl();",3000);
 </script>
--- send.php
+++ send.php
@@ -1,7 +1,7 @@
 <?php 
 if(isset($_POST['submit'])){
     /* Устанавливаем e-mail Кому и от Кого будут приходить письма */   
-    $to = "i@u53335.onhh.ru"; // Здесь нужно написать e-mail, куда будут приходить письма   
+    $to = "info@geoskyspb.ru"; // Здесь нужно написать e-mail, куда будут приходить письма   
     $from = $_POST['email'];
 
 $name = $_POST['name'];
@@ -24,11 +24,11 @@ $headers2 = "From:" . $to;
 mail($to,$subject,$mail_to_myemail,$headers);
 // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
 echo "Сообщение отправлено. Спасибо Вам " . $name . ", мы скоро свяжемся с Вами.";
-echo "<br /><br /><a href='http://u53335.onhh.ru/'>Вернуться на сайт.</a>";
+echo "<br /><br /><a href='https://geoskyspb.ru'>Вернуться на сайт.</a>";
 }
 ?>
 <!--Переадресация на главную страницу сайта, через 3 секунды-->
 <script language="JavaScript" type="text/javascript">
-function changeurl(){eval(self.location="http://u53335.onhh.ru/");}
+function changeurl(){eval(self.location="https://geoskyspb.ru");}
 window.setTimeout("changeurl();",3000);
 </script>
